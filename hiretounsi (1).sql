19cd4d762d9134733adf96b645b13dedfe43ae2a
Molka Ben Abdallah <molka.benabdallah@isamm.u-manouba.tn>
2026-05-05 14:03:49 +0200

    modifier sql

--- hiretounsi (1).sql
+++ hiretounsi (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : dim. 03 mai 2026 à 19:41
+-- Généré le : mar. 05 mai 2026 à 14:00
 -- Version du serveur : 10.4.32-MariaDB
 -- Version de PHP : 8.0.30
 
@@ -24,6 +24,22 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `candidatures`
+--
+
+CREATE TABLE `candidatures` (
+  `id` int(11) NOT NULL,
+  `job_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `cv_path` varchar(255) DEFAULT NULL,
+  `message` text DEFAULT NULL,
+  `status` varchar(20) DEFAULT 'pending',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `jobs`
 --
 
@@ -35,22 +51,24 @@ CREATE TABLE `jobs` (
   `city` varchar(100) DEFAULT NULL,
   `description` text DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `status` varchar(20) DEFAULT 'pending'
+  `status` varchar(20) DEFAULT 'pending',
+  `user_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Déchargement des données de la table `jobs`
 --
 
-INSERT INTO `jobs` (`id`, `title`, `company`, `salary`, `city`, `description`, `created_at`, `status`) VALUES
-(1, 'offre1', 'x', '1000', 'tunis', 'temps complet', '2026-05-03 15:22:32', 'rejected'),
-(2, 'test1', 'x', '5000', 'nabeul', 'test test', '2026-05-03 15:31:39', 'approved'),
-(3, 'test2', 'y', '2500', 'sfax', 'test est test ', '2026-05-03 15:40:51', 'approved'),
-(4, 'test3', 'a', '1000', 'tunis', 'test3test3test3test3test3test3', '2026-05-03 16:32:25', 'approved'),
-(5, 'full', 't', '8520', 'sfax', 'testttttttttttttttttttttt', '2026-05-03 16:58:03', 'approved'),
-(6, 'test', 'r', '1000', 'tunis', 'tessssssssssssssssssst', '2026-05-03 16:59:43', 'approved'),
-(7, 'designer', 's', '1500', 'gasrine', 'knatri', '2026-05-03 17:01:31', 'approved'),
-(8, 'e commerce', 'azerty', '1800', 'gasrine', 'kontra', '2026-05-03 17:24:44', 'rejected');
+INSERT INTO `jobs` (`id`, `title`, `company`, `salary`, `city`, `description`, `created_at`, `status`, `user_id`) VALUES
+(1, 'offre1', 'x', '1000', 'tunis', 'temps complet', '2026-05-03 15:22:32', 'rejected', NULL),
+(2, 'test1', 'x', '5000', 'nabeul', 'test test', '2026-05-03 15:31:39', 'approved', NULL),
+(3, 'test2', 'y', '2500', 'sfax', 'test est test ', '2026-05-03 15:40:51', 'approved', NULL),
+(4, 'test3', 'a', '1000', 'tunis', 'test3test3test3test3test3test3', '2026-05-03 16:32:25', 'approved', NULL),
+(5, 'full', 't', '8520', 'sfax', 'testttttttttttttttttttttt', '2026-05-03 16:58:03', 'approved', NULL),
+(6, 'test', 'r', '1000', 'tunis', 'tessssssssssssssssssst', '2026-05-03 16:59:43', 'approved', NULL),
+(7, 'designer', 's', '1500', 'gasrine', 'knatri', '2026-05-03 17:01:31', 'approved', NULL),
+(8, 'e commerce', 'azerty', '1800', 'gasrine', 'kontra', '2026-05-03 17:24:44', 'rejected', NULL),
+(9, 'jobs', 'h', '1000', 'tunis', 'jobs', '2026-05-04 07:56:44', 'approved', NULL);
 
 -- --------------------------------------------------------
 
@@ -80,7 +98,8 @@ INSERT INTO `talents` (`id`, `name`, `skill`, `description`, `status`, `user_id`
 (6, 'profil', 'pppp', 'pppppp', 'approved', 9),
 (7, 'molka ben abdallah', 'UI/UX,DESIGNER', 'ETUDIANTE', 'approved', 10),
 (8, 'zayd', 'devloppeur', 'ej suis full-stuck  dev', 'approved', 9),
-(9, 'test8', 'nodejs react,css,html', 'test88888888888888', 'pending', 10);
+(9, 'test8', 'nodejs react,css,html', 'test88888888888888', 'pending', 10),
+(10, 'molka', 'UI/UX REACT,REACT,JS,NODE', 'MOLKA ', 'approved', 11);
 
 -- --------------------------------------------------------
 
@@ -100,25 +119,30 @@ CREATE TABLE `users` (
   `cv_path` varchar(255) DEFAULT NULL,
   `avatar_path` varchar(255) DEFAULT NULL,
   `job_title` varchar(100) DEFAULT NULL,
-  `role` enum('user','admin') DEFAULT 'user'
+  `role` enum('candidat','recruteur','both','admin') DEFAULT 'candidat',
+  `is_recruiter` tinyint(1) DEFAULT 0,
+  `profile_photo` varchar(255) DEFAULT NULL,
+  `cv` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Déchargement des données de la table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `password`, `created_at`, `phone`, `bio`, `skills`, `cv_path`, `avatar_path`, `job_title`, `role`) VALUES
-(3, 'ali', 'a@gmail.com', '$2y$10$sMNrcTZ8irHHCNzxS8ohaei6xsPrUgdp52Zt1kGQ1wz9Vlg1n2ziK', '2026-04-29 18:25:16', NULL, NULL, NULL, NULL, NULL, NULL, 'user'),
-(5, 'molkaaaaaaaaaaaaaaa', 'molkaaa@gmail.com', '$2y$10$iTVyIiAmsJTwcn86djdNzuNSUSvOjUEMey53bld9GRdiiGFn.4f3y', '2026-05-01 09:42:26', NULL, NULL, NULL, NULL, NULL, NULL, 'user'),
-(6, 'waell', 'w@gmail.com', '$2y$10$RCM0gMoqqKKmhutziIjJt./cwgnnRTZHJkaDoPN6tBQPi9lQ8YLPq', '2026-05-01 11:11:46', NULL, NULL, NULL, NULL, NULL, NULL, 'user'),
-(7, 'mmmm', 'mm@gmail.com', '$2y$10$AJVxqXSvBxNjVRr9mt4jvOIdnn3V3kvAbGJBihCDGgY5wlz4YXib2', '2026-05-01 11:18:10', NULL, NULL, NULL, NULL, NULL, NULL, 'admin'),
-(8, 'molka', 'molka@gmail.com', '$2y$10$d613Ncin.uOM.HxQc/5iw.8aULVn08Rda/rzt7J65e/hQARJXIQ.m', '2026-05-02 08:40:10', NULL, NULL, NULL, NULL, NULL, NULL, 'user'),
-(9, 'mouhamed', 'mou@gmail.com', '$2y$10$s9BhDusFmupZsXLz/pdU/euLcO31jPPeLX79KZppMuPMzzsktJd/2', '2026-05-02 14:46:13', NULL, NULL, NULL, NULL, NULL, NULL, 'user'),
-(10, 'admin', 'admin@gmail.com', '$2y$10$xFPITaAIOh8kGobjdvfs5.dCN.67H4CfYz9bWp43XisjWH8eOauii', '2026-05-02 14:58:45', NULL, NULL, NULL, NULL, NULL, NULL, 'admin');
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `created_at`, `phone`, `bio`, `skills`, `cv_path`, `avatar_path`, `job_title`, `role`, `is_recruiter`, `profile_photo`, `cv`) VALUES
+(9, 'mouhamed', 'mou@gmail.com', '$2y$10$s9BhDusFmupZsXLz/pdU/euLcO31jPPeLX79KZppMuPMzzsktJd/2', '2026-05-02 14:46:13', '12342587', 'ui ux', 'ui ux', NULL, NULL, 'ui ux', 'both', 0, '1777981355_RED LOGO.png', '1777981355_rapport (1).pdf'),
+(10, 'admin', 'admin@gmail.com', '$2y$10$xFPITaAIOh8kGobjdvfs5.dCN.67H4CfYz9bWp43XisjWH8eOauii', '2026-05-02 14:58:45', NULL, NULL, 'ui/ux', NULL, NULL, NULL, 'admin', 0, '1777900608_Gemini_Generated_Image_k09pask09pask09p.png', '1777900608_Capture d\'écran 2026-05-04 130727.png'),
+(11, 'ben abdallah molka', 'benabdallahmolka13@gmail.com', '$2y$10$VaPfsT1HBMaeI8Paa1ZNqutbMHuFH1aSt/dWltGlEjVlTH1OFbodS', '2026-05-04 09:19:21', NULL, NULL, NULL, NULL, NULL, NULL, 'both', 0, NULL, NULL);
 
 --
 -- Index pour les tables déchargées
 --
+
+--
+-- Index pour la table `candidatures`
+--
+ALTER TABLE `candidatures`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `jobs`
@@ -144,22 +168,28 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT pour la table `candidatures`
+--
+ALTER TABLE `candidatures`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT pour la table `jobs`
 --
 ALTER TABLE `jobs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT pour la table `talents`
 --
 ALTER TABLE `talents`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
